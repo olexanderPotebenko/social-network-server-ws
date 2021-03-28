14cb5ec4e65ed8f497c0faf259f2688ac0e22032
olexanderPotebenko <olexander.potebenko@gmail.com>
2021-03-28 02:34:26 +0200

    уведомления о подписке и лайке

--- app/index.js
+++ app/index.js
@@ -8,6 +8,8 @@ let actions = {
   auth: 'AUTH',
   send_message: 'SEND-MESSAGE',
   read_messages: 'READ-MESSAGES',
+  follow: 'WS-FOLLOW',
+  liked: 'SET-LIKES-POST',
 }
 
 //main
@@ -27,6 +29,7 @@ wss.on('connection', function(ws) {
       message = JSON.parse(message);
 
 
+      let sended = false;
       console.log(message);
       switch(message.action) {
         case actions.auth:
@@ -39,9 +42,6 @@ wss.on('connection', function(ws) {
             ws.token = message.token;
             console.log(ws.token);
 
-            ws.on('message', function(message) {
-              ws.send(message);
-            });
           } else {
             let data = JSON.stringify({
               result_code: 1,
@@ -55,42 +55,89 @@ wss.on('connection', function(ws) {
           console.log(message);
           console.log('SEND MESSAGE');
           wss.clients.forEach(client => {
+            if(sended) return;
             console.log(client.id);
             console.log(message.user_id);
             console.log(client.readyState);
             console.log(WebSocket);
-            if(client.id == message.user_id && client.readyState == 1) {
-              console.log('SEND MESSAGE FOR CLIENT WITH ID: ' + client.id);
-            console.log(new Date().getMinutes());
-              let data = {
-                id: message.id,
-                action: actions.send_message,
+            if(client.id == message.user_id) {
+              if(client.readyState == 1) {
+                console.log('SEND MESSAGE FOR CLIENT WITH ID: ' + client.id);
+                console.log(new Date().getMinutes());
+                let data = {
+                  id: message.id,
+                  action: actions.send_message,
+                };
+                console.log(data);
+                sended = true;
+                client.send(JSON.stringify(data));
+              } else {
+                console.log('Client ' + client.id + 'not online');
               };
-              console.log(data);
-              client.send(JSON.stringify(data));
-            } else {
-              console.log('Client ' + client.id + 'not online');
-            };
+            }
           });
           break;
         case actions.read_messages:
           wss.clients.forEach(client => {
-            if(client.id === message.user_id && client.readyState == 1) {
-              console.log(`SEND FOR USER ID=${message.user_id} TO UPDATE DIALOG ID=${message.dialog_id}`);
-              let data = {
-                dialog_id: message.dialog_id,
-                action: actions.read_messages,
+            if(client.id === message.user_id) {
+              if (client.readyState == 1) {
+                console.log(`SEND FOR USER ID=${message.user_id} TO UPDATE DIALOG ID=${message.dialog_id}`);
+                let data = {
+                  dialog_id: message.dialog_id,
+                  action: actions.read_messages,
+                };
+                console.log(data);
+                client.send(JSON.stringify(data));
+              } else {
+                console.log('Client ' + client.id + 'not online');
               };
-              console.log(data);
-              client.send(JSON.stringify(data));
-            } else {
-              console.log('Client ' + client.id + 'not online');
-            };
+            }
           });
           console.log(message);
           break;
+        case actions.follow:
+          console.log(message);
+          wss.clients.forEach(client => {
+            if(sended) return;
+            if(client.id === message.user_id) {
+              if(client.readyState == 1) {
+                console.log(`SEND USER ID=${message.user_id} ABOUT USER ID=${message.id} TO FOLLOW`);
+                let data = {
+                  id: message.id,
+                  action: actions.follow,
+                };
+                console.log(data);
+                client.send(JSON.stringify(data));
+                sended = true;
+              } else {
+                console.log('Client ' + client.id + 'not online');
+              };
+            }
+          });
+          break;
+        case actions.liked:
+          console.log('-----------');
+          console.log(message);
+          wss.clients.forEach(client => {
+            if(sended) return;
+            if(client.id === message.user_id) {
+              if(client.readyState == 1) {
+                console.log(`SEND USER ID=${message.user_id} ABOUT USER ID=${message.id} LIKED YOU POST ID=${message.post_id}`);
+                let data = {
+                  id: message.id,
+                  action: actions.liked,
+                  post_id: message.post_id,
+                };
+                console.log(data);
+                client.send(JSON.stringify(data));
+                sended = true;
+              } else {
+                console.log('Client ' + client.id + 'not online');
+              };
+            }
+          });
       };
-    } catch(e){
+    } catch(e) {
       console.log(e);
       let data = JSON.stringify({
         result_code: 1,
